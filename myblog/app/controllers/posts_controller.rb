f5d6ed8a82e525281e05cf1db690989171489816
Xion7 <xhunterrx@yahoo.co.jp>
2019-05-25 22:00:24 +0900

    complete

--- myblog/app/controllers/posts_controller.rb
+++ myblog/app/controllers/posts_controller.rb
@@ -8,6 +8,7 @@ class PostsController < ApplicationController
   end
 
   def new
+    @post = Post.new
   end
 
   def create
@@ -15,8 +16,31 @@ class PostsController < ApplicationController
     #save
     #@post = Post.new(params[:post])
     @post = Post.new(post_params)
-    @post.save
+    if @post.save
     #redirect
+      redirect_to posts_path
+    else
+      #render plain: @post.errors.inspect
+      render 'new'
+    end
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      redirect_to posts_path
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
     redirect_to posts_path
   end
 
